6adf562b688e6774db7db1342f9479be67e51879
AlexandrKryu <a.kryu84@gmail.com>
2022-09-01 22:53:20 +0300

    Add Task 47,50,52

---
+++ Task50/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента
+//  в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+Console.WriteLine("Введите координаты элемента по строке:");
+int elemRow = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координаты элемента по столбцу: ");
+int elemCol = int.Parse(Console.ReadLine()!);
+
+
+int[,] CreateMtrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) System.Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void PrintMatrixElem(int[,] matrix)
+{
+if (elemRow < 1 || elemRow > matrix.GetLength(0) || elemCol < 1 || elemCol > matrix.GetLength(1))
+{
+  Console.Write("Элемента нет");
+}
+else
+{
+  Console.Write($"Элемент = {matrix[elemRow-1, elemCol-1]}");
+}   
+}
+int[,] matrix = CreateMtrixRndInt(3, 5, 2, 20);
+PrintMatrix(matrix);
+PrintMatrixElem(matrix);
---
+++ Task52/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMtrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) System.Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[] MeanElemCol(int[,] array)
+{
+    int col = array.GetLength(1);
+    int[] result = new int[col];
+    // double[] res = new double[result.Length];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            if (i<array.GetLength(0))
+                result[col] += array[i, j];
+            else
+                result[result.Length] += result[col] / i;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int[,] matrix = CreateMtrixRndInt(4, 4, 1, 10);
+PrintMatrix(matrix);
+int[] array = MeanElemCol(matrix);
+PrintArray(array);
+
